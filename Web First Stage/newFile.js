3430c6356e3055434f01c1325b3a9ad502646d9f
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-04-16 13:12:16 +0530

    added js

---
+++ Web First Stage/newFile.js
@@ -0,0 +1,23 @@
+var x = 10;
+var name = "Alexa"
+let gender =" Male"
+const institute="x workz"
+document.writeln(age)
+let emp1 = {id:12,EmployeName:"Vinayaka",salary:10000}
+let emp2 = new Object();
+emp2.id = 13;
+emp2.EmployeName="Sahithya";
+emp2.salary=12000;
+emp2.id=14;
+
+function emp3(id,EmployeName,salary){
+    this.id=id;
+    this.EmployeName=EmployeName;
+    this.salary=salary;
+}
+
+let obj3 = emp3(satisfies,satisfies,s);
+
+
+
+
