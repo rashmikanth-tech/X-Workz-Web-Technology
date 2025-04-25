f511666e8be89ffa4e3c1dffb083cf21a94345ce
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-04-25 13:29:49 +0530

    updated workform

--- Web First Stage/workform.js
+++ Web First Stage/workform.js
@@ -2,17 +2,37 @@ function onClick() {
     alert("form is Submitted")
 }
 function submitForm() {
-    let value = true;
+    let valu = true;
     let nameInput = document.formName.nameName.value;
     let numInput = document.formName.numberNumber.value;
     let gendInput = document.formName.Gender.value;
     let ageInput = document.formName.ageAge.value;
     let emailinput = document.formName.emailEmail.value;
 
-    alert(nameInput)
-    alert(numInput)
-    alert(gendInput)
-    alert(ageInput)
-    alert(emailinput)
-    return value;
+  
+
+    if (numInput.length > 10 && numInput.length < 30) {
+        valu = true;
+    }
+    else {
+        valu = false;
+        alert("Invalid Number")
+    }
+    if (nameInput.length > 6 && nameInput.length < 30) {
+        valu = true;
+    }
+    else {
+        valu = false;
+        alert("Name Length Not Match")
+    }
+
+    if (ageInput > 18 && ageInput < 70) {
+        valu = true;
+    }
+    else {
+        valu = false;
+        alert("Enter valid Age")
+    }
+    return valu
+
 }
