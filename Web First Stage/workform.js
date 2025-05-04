3a74e6ca442785f3b81041f610d41986237c1b49
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-05-04 19:24:01 +0530

    changes made in workform

--- Web First Stage/workform.js
+++ Web First Stage/workform.js
@@ -2,49 +2,55 @@ function onClick() {
     
    let doThis = document.getElementsByClassName("numClass")[0].innerHTML='Please provide Minimum Number'
   console.log('done now',doThis)
+}
 
-  document.getElementsByClassName("yenriMedia")[0].innerHTML;
-  document.getElementsByClassName("YakriMedia")[0].innerHTML;
-}
-}
 function submitForm() {
     let valu = true;
     let nameInput = document.formName.nameName.value;
     let numInput = document.formName.numberNumber.value;
-    let gendInput = document.formName.Gender.value;
-    let ageInput = document.formName.ageAge.value;
-    let emailinput = document.formName.emailEmail.value;
+   // let gendInput = document.formName.Gender.value;
+    //let ageInput = document.formName.ageAge.value;
+    //let emailinput = document.formName.emailEmail.value;
+    
 
   
 
-    if (numInput.length > 10 && numInput.length < 30) {
-        valu = true;
-        errorcheck();
+    if (numInput.length !=10) {
+        valu = false;
+        errorcheck("numError", "number should be at least 10 characters max 30");
     }
     else {
-        valu = false;
-        alert("Invalid Number")
+       
+        clearField('numError');
+
     }
-    if (nameInput.length > 6 && nameInput.length < 30) {
-        valu = true;
-        errorcheck();
-    }
-    else {
+   
+    if (nameInput.length <3|| nameInput.length >30) {
         valu = false;
-        alert("Name Length Not Match")
+        errorcheck("nameError", "Name should be at least 3 characters");
+    }
+    
+    else{
+       
+        clearField('nameError');
     }
 
-    if (ageInput > 18 && ageInput < 70) {
-        valu = true;
-        errorcheck();
-    }
-    else {
-        valu = false;
-        alert("Enter valid Age")
-    }
-    return valu
+
+    //if (ageInput > 18 && ageInput < 70) {
+      //  valu = true;
+        //errorcheck();
+    //}
+   // else {
+     //   valu = false;
+       // alert("Enter valid Age")
+    //}
+    return valu;
 
 }
-function errorcheck(){
-    alert("error is working")
+function errorcheck(id,message){
+    document.getElementById(id).innerHTML = message;
+   
+}
+function clearField(id){
+    document.getElementById(id).innerHTML='';
 }
